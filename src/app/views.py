828f544e0857fb5076b76cb6178318468cbcc098
g-fringer <gage@fringer.us>
2022-02-11 15:47:36 -0500

    Made initial frontend pages.
    This currently has the frontend run separately, will integrate this with docker-compose shortly.

--- src/app/views.py
+++ src/app/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, jsonify
+from flask_cors import CORS, cross_origin
 views = Blueprint('views', __name__, template_folder='templates')
 
 #Routes for web pages
 
 @views.route('/')
+@cross_origin()
 def index():
-    return render_template('index.html')
+    return jsonify(render_template('index.html'))
