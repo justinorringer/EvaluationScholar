36ad2ae5d86e60cb3dcad96e93b8ad5265b5828c
tcmaxwel <tcmaxwel@ncsu.edu>
2022-02-28 23:06:21 -0500

    Add scheduler

--- backend/factory.py
+++ backend/factory.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 def create_app(session):
     app = Flask(__name__)
@@ -12,3 +13,11 @@ def create_app(session):
     app.session = session
 
     return app
+
+def create_connection_string():
+    db_host = str(os.environ.get('DB_HOST'))
+    db_user = str(os.environ.get('DB_USER'))
+    db_pass = str(os.environ.get('DB_PASSWORD'))
+    db_database = str(os.environ.get('DB_DATABASE'))
+
+    return f"mysql://{db_user}:{db_pass}@{db_host}:3306/{db_database}"
