a9da398a820ef7e6149d7dbe39806d8c2e22995f
gwfringe <gwfringe@ncsu.edu>
2022-03-11 12:12:45 -0500

    (gwfringe) Resolved border issue on issues/jobs pages

--- frontend/src/components/issues.jsx
+++ frontend/src/components/issues.jsx
@@ -8,12 +8,15 @@ function Issues() {
         <div className="container pt-4">
             <h2>Issues</h2>
         </div>
-        <div className="container border my-3 p-4">
+        <div className="container my-3 p-4 border border-5 border-dark">
             {/**<button type="button" className="close btn-small float-right" aria-label="Close">&times;</button>*/}
 
             <h3>Ambiguous Search</h3>
 
             <p>Inputed Title: Ambiguous</p><br />
+
+            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
+            <button type="button" className="btn btn-danger btn-sm float-right pl-1">Dismiss</button>
 
             <h5>Select the Best Match</h5>
             <input type="radio" aria-label="Checkbox for following text input" /> Option 1<br />
@@ -21,24 +24,24 @@ function Issues() {
             <input type="radio" aria-label="Checkbox for following text input" /> Option 3 <br />
             <input type="radio" aria-label="Checkbox for following text input" /> None of the Suggested <br /><br />
 
-            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
-            <button type="button" className="btn btn-danger btn-sm float-right pl-1">Dismiss</button>
         </div>
 
-        <div className="container border my-3 p-4">
+        <div className="container my-3 p-4 border border-5 border-dark">
             <h3>Paper Not Found</h3>
 
             <p>Inputed Title: Paper?</p><br />
 
             <input type="text" className="form-control" id="validationServer01" placeholder="Correct Title" required></input>
+            
+            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
+            <button type="button" className="btn btn-danger btn-sm float-right">Dismiss</button>
+            
             <div className="valid-feedback">
                 Looks good!
             </div>
 
             <br />
-
-            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
-            <button type="button" className="btn btn-danger btn-sm float-right">Dismiss</button>
+            
         </div>
     </div>
   );
--- frontend/src/components/jobs.jsx
+++ frontend/src/components/jobs.jsx
@@ -8,23 +8,25 @@ function Jobs() {
         <div className="container pt-3">
             <h1>Queued Jobs</h1>
         </div>
-        <div className="container border-warning my-3 p-4">
+        <div className="container my-3 p-4 border border-5 border-dark">
             <h3>Job Type</h3>
+
+            <button type="button" className="btn btn-danger btn-sm float-right">Cancel Job</button>
 
             <p>Importance: 0</p>
             <p>Date Scheduled: <b>Bleh</b></p>
 
-            <button type="button" className="btn btn-danger btn-sm float-right">Cancel Job</button>
         </div>
-        <div className="container border-warning my-3 p-4">
+        <div className="container my-3 p-4 border border-5 border-dark">
             {/* <button type="button" className="close btn-small float-right" aria-label="Close">&times;</button> -->*/}
 
             <h3>Job Type</h3>
 
+            <button type="button" className="btn btn-danger btn-sm float-right">Cancel Job</button>
+
             <p>Importance: 0</p>
             <p>Date Scheduled: <b>Bleh</b></p>
-
-            <button type="button" className="btn btn-danger btn-sm float-right">Cancel Job</button>
+            
         </div>
     </div>
   );
