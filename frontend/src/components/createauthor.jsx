76b0aa33b85babdfc2ec10325d888c6a0b4669a0
csthunes <csthunes@ncsu.edu>
2022-03-30 10:54:03 -0400

    Repushing files for git fix.

---
+++ frontend/src/components/createauthor.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import axios from 'axios';
+
+//Authors: Carter Thunes, Justin Orringer
+function CreateAuthor() {
+    
+    let authors = [];
+
+    //Function to scrape authors in google scholar.
+    const getScrapedAuthors = async () => {
+        document.getElementById("wait").innerText = "Searching...";
+        let name = document.getElementById("authName").value;
+        console.log("Awaiting response for search");
+        const response = await axios.get(`/api/scraping/profiles?name=${name}`, {
+            mode:'cors'});
+        if (response.status === 200) {
+            console.log(response);
+            authors = response.data;
+            const parentList = document.getElementById("authorList");
+            parentList.innerHTML = "";
+            console.log(authors);
+            authors.forEach(author => {
+                console.log(author);
+                let a = document.createElement("a");
+                a.className = "list-group-item list-group-item-action flex-column align-items-start";
+                a.id = author.name + '+' + author.id;
+                a.onclick = function() { createAuthor(a.id) };
+                let div = document.createElement("div");
+                div.className = "d-flex w-100 justify-content-between";
+                let h5 = document.createElement("h5");
+                h5.className = "mb-1 py-2";
+                h5.innerText = author.name;
+                let small = document.createElement("small");
+                small.className = "text-muted";
+                small.innerText = author.institution;
+                parentList.appendChild(a);
+                a.appendChild(div);
+                div.appendChild(h5);
+                div.appendChild(small);
+            });
+        }
+        document.getElementById("wait").innerText = "";
+    }
+    
+    function createAuthor(name_id) {
+        const arr = name_id.split("+");
+        console.log(arr);
+        //Function to create an author in the database through an axios call.
+        const makeAuthor = async () => {
+            const response = await axios({
+            method: "post",
+            url: '/api/authors',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+
+            },
+            
+            //make sure to serialize your JSON body
+            data: JSON.stringify({
+                name: arr[0],
+                scholar_id: arr[1]
+            }), mode: 'cors'
+            }, true);
+            
+            console.log(response);
+            const authorID = response.data.id;
+            window.location.href = `/author/${authorID}`;
+        }
+        makeAuthor();
+    }
+
+  //Return the related HTML of the page.
+  return (
+    <div className="body">
+        <div className="container">
+            <div className="row pl-3">
+                <label>Enter Author Name: &nbsp;</label>
+                <input className="mx-3" type="text" id="authName"></input>
+                <button type="button" class="btn btm-sm btn-danger" onClick={getScrapedAuthors}>Search Google Scholar</button>
+                <span className="ml-3" id="wait"></span>
+            </div>
+        </div>
+
+        <div className="container pt-4" id="optionsLabel">
+            <h4>Author Options:</h4>
+        </div>
+        <div className="container border my-3 p-4">
+            <div className="list-group" id="authorList">
+            </div>
+        </div>
+    </div>
+    
+  );
+}
+
+export default CreateAuthor;
---
+++ frontend/src/components/issues.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from 'axios';
+
+function Issues() {
+  //Return the related HTML of the page.
+  return (
+    <div className="body">
+        <div className="container">
+            <h3>Issues</h3>
+        </div>
+        <div className="container my-3 p-4 border border-5 border-dark">
+            {/**<button type="button" className="close btn-small float-right" aria-label="Close">&times;</button>*/}
+
+            <h3>Ambiguous Search</h3>
+
+            <p>Inputed Title: Ambiguous</p><br />
+
+            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
+            <button type="button" className="btn btn-danger btn-sm float-right pl-1">Dismiss</button>
+
+            <h5>Select the Best Match</h5>
+            <input type="radio" aria-label="Checkbox for following text input" /> Option 1<br />
+            <input type="radio" aria-label="Checkbox for following text input" /> Option 2<br />
+            <input type="radio" aria-label="Checkbox for following text input" /> Option 3 <br />
+            <input type="radio" aria-label="Checkbox for following text input" /> None of the Suggested <br /><br />
+
+        </div>
+
+        <div className="container my-3 p-4 border border-5 border-dark">
+            <h3>Paper Not Found</h3>
+
+            <p>Inputed Title: Paper?</p><br />
+
+            <input type="text" className="form-control" id="validationServer01" placeholder="Correct Title" required></input>
+            
+            <button type="button" className="btn btn-success btn-sm float-right">Correct</button>
+            <button type="button" className="btn btn-danger btn-sm float-right">Dismiss</button>
+            
+            <div className="valid-feedback">
+                Looks good!
+            </div>
+
+            <br />
+            
+        </div>
+    </div>
+  );
+}
+
+export default Issues;
